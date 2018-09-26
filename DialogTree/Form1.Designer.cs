fd729390197be82a508545736277c2c12f5275c6
TominoCZ <TominoCZ@users.noreply.github.com>
2018-09-26 18:44:22 +0200

    Initial commit

---
+++ DialogTree/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace DialogTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbAnswers = new System.Windows.Forms.ListBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnAnswer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbAnswers
+            // 
+            this.lbAnswers.FormattingEnabled = true;
+            this.lbAnswers.ItemHeight = 21;
+            this.lbAnswers.Location = new System.Drawing.Point(18, 42);
+            this.lbAnswers.Margin = new System.Windows.Forms.Padding(5);
+            this.lbAnswers.Name = "lbAnswers";
+            this.lbAnswers.Size = new System.Drawing.Size(253, 88);
+            this.lbAnswers.TabIndex = 0;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(14, 9);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 21);
+            this.lblMessage.TabIndex = 1;
+            // 
+            // btnAnswer
+            // 
+            this.btnAnswer.Location = new System.Drawing.Point(18, 138);
+            this.btnAnswer.Name = "btnAnswer";
+            this.btnAnswer.Size = new System.Drawing.Size(253, 32);
+            this.btnAnswer.TabIndex = 2;
+            this.btnAnswer.Text = "Next";
+            this.btnAnswer.UseVisualStyleBackColor = true;
+            this.btnAnswer.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 182);
+            this.Controls.Add(this.btnAnswer);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lbAnswers);
+            this.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbAnswers;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnAnswer;
+    }
+}
+
---
+++ DialogTree/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogTree
+{
+    public partial class Form1 : Form
+    {
+        private DialogTree _dialogTree;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _dialogTree = new DialogTree();
+
+            var m1 = new Message("Hi. How's it going?");
+            var m2 = new Message("That's great!");
+            var m3 = new Message("Well that sucks..");
+
+            var a1 = new Answer("Ok", m2);
+            var a2 = new Answer("Not Ok", m3);
+
+            m1.Answers = new[] { a1, a2 };
+
+            _dialogTree.SetTree(m1);
+
+            UpdateDialog();
+        }
+
+        private void UpdateDialog()
+        {
+            var active = _dialogTree.GetCurrent();
+
+            if (active == null)
+                return;
+
+            if (active.Answers.Length == 0)
+                _dialogTree.Reset();
+            
+            lblMessage.Text = active.Text;
+
+            lbAnswers.Items.Clear();
+
+            foreach (var answer in active.Answers)
+            {
+                lbAnswers.Items.Add(answer.Text);
+            }
+        }
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            var selected = lbAnswers.SelectedIndex;
+
+            _dialogTree.Answer(selected);
+
+            UpdateDialog();
+        }
+    }
+
+    class DialogTree
+    {
+        private Message _first;
+
+        public void SetTree(Message q)
+        {
+            _first = q;
+        }
+
+        public Message GetCurrent()
+        {
+            Message current = _first;
+
+            while (current != null && current.Chosen != -1)
+            {
+                current = current.Next;
+            }
+
+            return current;
+        }
+
+        public void Answer(int index)
+        {
+            var current = GetCurrent();
+
+            if (current == null)
+                return;
+
+            current.Chosen = index;
+        }
+
+        public void Reset()
+        {
+            Message current = _first;
+
+            while (current != null && current.Chosen != -1)
+            {
+                current.Chosen = -1;
+                current = current.Next;
+            }
+        }
+    }
+
+    class Message
+    {
+        public string Text;
+
+        public Answer[] Answers;
+
+        public Message Next => Chosen == -1 ? null : Answers[Chosen].Next;
+
+        public int Chosen = -1;
+
+        public Message(string text, params Answer[] answers)
+        {
+            Text = text;
+            Answers = answers;
+        }
+    }
+
+    class Answer
+    {
+        public string Text;
+
+        public Message Next;
+
+        public Answer(string text, Message next)
+        {
+            Text = text;
+            Next = next;
+        }
+    }
+}
